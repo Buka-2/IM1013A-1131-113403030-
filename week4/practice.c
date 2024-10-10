8e98a025d5c9ae5e93af87f640cfc864e50ceec8
Buka-2 <dkchan@g.ncu.edu.tw>
2024-10-10 21:26:21 +0000

    perfect

--- week4/practice.c
+++ week4/practice.c
@@ -1,28 +1,40 @@
 #include<stdio.h>
 int main(void){
     int NumOfStudents;
-    int Countdown;
+    int countdown;
     int scoreofstudent;
     int countofstudent=1;
-    float total;
+    float total=0;
     float average;
 
 
-    Countdown=0;
+    countdown=0;
     puts("please tell me how many grades do you want to calculate");
     scanf("%d",&NumOfStudents);
-    while(Countdown<NumOfStudents){
-        printf("please enter the score of the number %d student\n",countofstudent);
-        countofstudent=countofstudent+1;
-        scanf("%d",&scoreofstudent);
-        total=total+scoreofstudent;
-        Countdown=Countdown+1;
+    if(NumOfStudents<0){
+        puts("the number of the students iss not valid");
     }
+    else{
+    while(countdown<NumOfStudents){
+        printf("please enter the score of the number %d student\n",countofstudent);
+        scanf("%d",&scoreofstudent);
+        if(scoreofstudent<0){
+            countdown=countdown+100;
+            puts("the score is not valid");}
+            if(scoreofstudent>100){
+                countdown=countdown+100;
+                puts("the score is not valid");
+            }
+        countofstudent=countofstudent+1;
+        total=total+scoreofstudent;
+        countdown=countdown+1;
+    }
+    if(countdown<50){
+    
     average=total/NumOfStudents;
     printf("the total score of students is %f \n",total);
     printf("the average of the students if %f \n",average);
-
+    }}}
     /*average=total/NumOfStudents;
     printf("the average of the students is %F",average);
     */
-}
